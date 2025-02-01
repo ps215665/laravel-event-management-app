1680a3014cde822dc0f87ae562bc8b5136309833
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-01 21:40:27 +0530

    changesi n attendee model

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -14,6 +14,11 @@ class EventResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'user' => new UserResource($this->whenLoaded('user'))
+        ];
     }
 }
