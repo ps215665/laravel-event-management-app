d03865237f50adf25e2a3a364434aac8b7425e07
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-01 21:41:20 +0530

    protected routes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('events', \App\Http\Controllers\Api\EventController::class);
+Route::apiResource('events', \App\Http\Controllers\Api\EventController::class)->middleware('auth:sanctum')->except(['index', 'show']);
 Route::apiResource('events.attendees', \App\Http\Controllers\Api\AttendeeController::class)
-    ->scoped(['attendee' => 'event']);
+    ->scoped()->except(['update']);
+
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');;
 
