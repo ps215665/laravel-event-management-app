87715a81a84da2293e3865eecc15308e95e7dbca
Prakul Sharma <176670611+ps215665@users.noreply.github.com>
2025-02-01 17:31:26 +0530

    Added controllers and changes in api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('events', \App\Http\Controllers\Api\EventController::class);
+Route::apiResource('events.attendees', \App\Http\Controllers\Api\AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
+
